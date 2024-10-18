35f1ea772b404bf41a80f82c5cc8a50879deb71a
Zoltan Belicza <zoltan.belicza@up4.hu>
2024-10-18 12:37:43 +0200

    design: adding all necessary variations as card component

--- src/app/components/shared/card/card.component.ts
+++ src/app/components/shared/card/card.component.ts
@@ -1,8 +1,46 @@
-import { Component } from '@angular/core';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    providers: [
+        {
+            provide: STEPPER_GLOBAL_OPTIONS,
+            useValue: { displayDefaultIndicatorType: false },
+        },
+    ],
 })
-export class CardComponent {}
+export class CardComponent implements OnInit {
+    firstFormGroup = this._formBuilder.group({
+        firstCtrl: [''],
+    });
+    secondFormGroup = this._formBuilder.group({
+        firstCtrl: [''],
+    });
+    thirdFormGroup = this._formBuilder.group({
+        firstCtrl: [''],
+    });
+    fourthFormGroup = this._formBuilder.group({
+        firstCtrl: [''],
+    });
+    fifthFormGroup = this._formBuilder.group({
+        firstCtrl: [''],
+    });
+    sixthFormGroup = this._formBuilder.group({
+        firstCtrl: [''],
+    });
+
+    public singleToggled: boolean = true;
+    constructor(private _formBuilder: FormBuilder) {}
+    public toggleSingle() {
+        return !this.singleToggled;
+    }
+
+    ngOnInit(): void {
+        this.singleToggled = true;
+    }
+}
