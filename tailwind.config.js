d3dfc70bd7e97ed576b4d96d240a3097458dc1d2
Zoltan Belicza <zoltan.belicza@gmail.com>
2025-03-13 14:17:12 +0100

    refactor: layouts refactored for proper responsive designs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     theme: {
         extend: {
             colors: {
+                // can be removed later on
                 transparent: "transparent",
                 current: "currentColor",
                 "blue-light": "#3B9FF7",
@@ -21,7 +22,50 @@ module.exports = {
                 "red-dark": "#FE2F20",
                 "yellow-base": "#E2BE58",
                 "text-default": "#EBEDF1",
-                // Light mode colors
+
+                primaryBlue: {
+                    DEFAULT: "#2293F7", // blue-base
+                    hover: "#3B9FF7", // blue-light
+                    focus: "#0877D9", // blue-dark
+                    disabled: "#A5C9F5", // inaktív állapotban halványított
+                    content: "#E2BE58", // tartalom szín: sárga (#E2BE58)
+                },
+                primaryGray: {
+                    DEFAULT: "#697492", // gray-base
+                    hover: "#8A9BB8", // enyhén világosabb árnyalat hover esetén
+                    focus: "#4D5A79", // sötétebb, fókusz állapot
+                    disabled: "#B0B7C6", // disabled állapot: halványított
+                    content: "#EBEDF1", // tartalom szín: szürkén kék (text-default)
+                },
+                success: {
+                    DEFAULT: "#06AC84",
+                    hover: "#08D9A8",
+                    focus: "#04765C",
+                    disabled: "#A5D4B3",
+                    content: "#FFFFFF",
+                },
+                error: {
+                    DEFAULT: "#FE5448",
+                    hover: "#FE675D",
+                    focus: "#FE2F20",
+                    disabled: "#FBB7B2",
+                    content: "#FFFFFF",
+                },
+                warning: {
+                    DEFAULT: "#E2BE58",
+                    hover: "#F0CF70",
+                    focus: "#CDA14E",
+                    disabled: "#F5E1B9",
+                    content: "#22262F",
+                },
+                text: {
+                    DEFAULT: "#22262F", // világos háttéren sötét szöveghez
+                    alternative: "#EBEDF1", // sötét háttéren világos szöveghez
+                },
+                background: {
+                    DEFAULT: "#FFFFFF", // alap háttérszín
+                },
+                // can be removed later on
                 primary: {
                     DEFAULT: "#6d28d9", // violet-700
                     focus: "#5b21b6", // violet-800
